232efc25babbadc7ab10a79df25a954154b4ccbe
assafvayner <assafvayner@gmail.com>
2020-03-06 22:34:35 -0800

    resize result figure

--- monthly.py
+++ monthly.py
@@ -6,19 +6,20 @@ import numpy as np
 
 def graph_months(df):
     sns.set()
-    fig, axs = plt.subplots(3, 4)#, xlim=(0, 32), ylim=(0, 300))
-    # for row in axs:
-    #     for ax in row:
-    #         ax.set(xlim=(0, 32), ylim=(0, 300))
+    fig, axs = plt.subplots(6, 4, figsize=(40, 60))
     gb = df.groupby('MONTH')
     gb = dict(list(gb))
     for month in range(12):
         month_df = gb[month + 1]
         ax = axs[month // 4][month % 4]
         data = month_df.groupby('DAY', as_index=False)[month_df.columns[:5]].mean()
-        data.plot(x='DAY', y='Total', ax=ax, kind='line')
+        # data.plot(x='DAY', y='Total', ax=ax, kind='line')
         ax.set_title(month_name(month, False))
-        # sns.lineplot(x='DAY', y='Total', data=data, ax=axs[month % 3][month % 4], size=100)
+        sns.barplot(x='DAY', y='Total', data=data, ax=ax)#, size=100)
+        ax = axs[(month + 12) // 4][(month + 12) % 4]
+        ax.set_title(month_name(month, False))
+        ax.xlabel()
+        sns.lineplot(x='DAY', y='Total', data=data, ax=ax)
     fig.savefig('plots/monthly.png')
 
 
@@ -34,15 +35,21 @@ def month_name(n, Jan_is_One=True):
             'July', 'August', 'September', 'October', 'November',
             'December')[n - Jan_is_One]
 
-
-def graph_history(df):
-    sns.set()
+def lowest_per_month(df, month=None):
+    if month is None:
+        res = dict()
+        gb = df.groupby('MONTH')
+        gb = dict(list(gb))
+        for m in range(12):
+            month_df = gb[m + 1]
+            v = month_df['Total'].min()
+            print(v)
 
 def main():
     dg = DataGetter()
     trail_data = dg.get_trail_data()
-    graph_months(trail_data)
-    graph_history(trail_data)
+    # graph_months(trail_data)
+    lowest_per_month(trail_data)
 
 
 if __name__ == '__main__':
