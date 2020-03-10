d73ca95835d6cf5d4ead3362808f6c47d02adc7f
assafvayner <assafvayner@gmail.com>
2020-03-10 16:51:04 -0700

    fix plot

--- daily.py
+++ daily.py
@@ -8,7 +8,7 @@ import seaborn as sns
 def graph_by_hour(data):
     data = data.groupby('HOUR', as_index=False).mean()
     fig, ax = plt.subplots(1, figsize=(6, 4))
-    sns.catplot(ax=ax, x='HOUR', y='Total', data=data, kind='bar')
+    sns.barplot(ax=ax, x='HOUR', y='Total', data=data)
     ax.set_title('People on the Burke Gillman Trail by Hour')
     ax.set_xlabel('Hour')
     ax.set_ylabel('Number of People')
